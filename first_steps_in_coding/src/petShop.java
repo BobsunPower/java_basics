edb15e4404243e927dfc3ecc9eb6e222e42d5a54
BobsunPower <bobsunpower@gmail.com>
2022-04-16 08:58:22 +0300

    Complete

--- first_steps_in_coding/src/petShop.java
+++ first_steps_in_coding/src/petShop.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class petShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dogs = Integer.parseInt(scanner.nextLine());
+        int cats = Integer.parseInt((scanner.nextLine()));
+        double dogFood = 2.50;
+        double catFood = 4;
+        double total = (dogs * dogFood) + (cats * catFood);
+        System.out.printf("%.2f lv.", total);
+    }
 }
--- first_steps_in_coding/src/yardGreening.java
+++ first_steps_in_coding/src/yardGreening.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class yardGreening {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double squareMeters = Double.parseDouble(scanner.nextLine());
+        double price = squareMeters * 7.61;
+        double discount = price * 0.18;
+        double total = price - discount;
+        System.out.printf("The final price is: %.2f lv.%n", total);
+        System.out.printf("The discount is: %.2f lv.", discount);
+    }
 }
