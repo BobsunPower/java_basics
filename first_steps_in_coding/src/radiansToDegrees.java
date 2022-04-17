389606cb5be71bfab2a69cd452dcfff5a9e3dd8e
BobsunPower <bobsunpower@gmail.com>
2022-04-17 10:56:26 +0300

    Complete

--- first_steps_in_coding/src/radiansToDegrees.java
+++ first_steps_in_coding/src/radiansToDegrees.java
@@ -1,2 +1,8 @@
+import java.util.Scanner;
+
 public class radiansToDegrees {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(Double.parseDouble(scanner.nextLine()) * 180 / Math.PI);
+    }
 }
