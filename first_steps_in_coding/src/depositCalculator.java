51a82a1b1b38ed5bdbfca44d908a9b96f8572495
BobsunPower <bobsunpower@gmail.com>
2022-04-18 21:31:51 +0300

    Completed

--- first_steps_in_coding/src/depositCalculator.java
+++ first_steps_in_coding/src/depositCalculator.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class depositCalculator {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        double deposit = Double.parseDouble(scanner.nextLine());
+        int term = Integer.parseInt(scanner.nextLine());
+        double interest = Double.parseDouble(scanner.nextLine());
+        System.out.println(deposit + (deposit * (interest / 100) / 12 * term));
+    }
 }
