0599be0d1dd08cfcae69b41e7090336437cfbe89
BobsunPower <bobsunpower@gmail.com>
2022-04-19 08:16:20 +0300

    Completed

--- first_steps_in_coding/src/vacationBooksList.java
+++ first_steps_in_coding/src/vacationBooksList.java
@@ -1,2 +1,8 @@
+import java.util.Scanner;
+
 public class vacationBooksList {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(Integer.parseInt(scanner.nextLine()) / Integer.parseInt(scanner.nextLine()) / Integer.parseInt(scanner.nextLine()));
+    }
 }
