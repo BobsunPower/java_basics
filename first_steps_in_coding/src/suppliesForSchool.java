39ce155b0adccc78fe5f1992174fb70c5794c18c
BobsunPower <bobsunpower@gmail.com>
2022-04-20 07:11:00 +0300

    Completed

--- first_steps_in_coding/src/suppliesForSchool.java
+++ first_steps_in_coding/src/suppliesForSchool.java
@@ -1,2 +1,8 @@
+import java.util.Scanner;
+
 public class suppliesForSchool {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(((Integer.parseInt(scanner.nextLine()) * 5.8) + (Integer.parseInt(scanner.nextLine()) * 7.2) + (Integer.parseInt(scanner.nextLine()) * 1.2)) * ((100 - Double.parseDouble(scanner.nextLine())) / 100));
+    }
 }
