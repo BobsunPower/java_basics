e3b29826388f82de8897f3cd1aa09e305b280a0b
BobsunPower <bobsunpower@gmail.com>
2022-04-21 06:28:25 +0300

    Completed

--- first_steps_in_coding/src/repainting.java
+++ first_steps_in_coding/src/repainting.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class repainting {
+    public static void main(String[] arg){
+        Scanner scanner = new Scanner(System.in);
+        double nylonPrice = 1.5;
+        double paintPrice = 14.5;
+        double separatorPrice = 5;
+        double bagsPrice = 0.4;
+        int nylonQuantity = Integer.parseInt(scanner.nextLine()) + 2;
+        double paintQuantity = Double.parseDouble((scanner.nextLine())) * 1.1;
+        int separatorQuantity = Integer.parseInt(scanner.nextLine());
+        int laborHours = Integer.parseInt(scanner.nextLine());
+        double stockPrice = (nylonQuantity * nylonPrice) + (paintQuantity * paintPrice) + (separatorQuantity * separatorPrice) + bagsPrice;
+        double laborPrice = stockPrice * 0.3;
+        double total = laborHours * laborPrice + stockPrice;
+        System.out.println(total);
+    }
 }
