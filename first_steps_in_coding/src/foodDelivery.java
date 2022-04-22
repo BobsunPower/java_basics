74d9d81bfb7fbcbb97e2fb92866856c7e9e040a6
BobsunPower <bobsunpower@gmail.com>
2022-04-22 08:09:02 +0300

    Completed

--- first_steps_in_coding/src/foodDelivery.java
+++ first_steps_in_coding/src/foodDelivery.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class foodDelivery {
+    public static void main(String[] arg){
+        Scanner scanner = new Scanner(System.in);
+        double chickenMenu = 10.35 * Double.parseDouble(scanner.nextLine());
+        double fishMenu = 12.4 * Double.parseDouble(scanner.nextLine());
+        double vegMenu = 8.15 * Double.parseDouble(scanner.nextLine());
+        double total = (chickenMenu + fishMenu + vegMenu) * 1.2 + 2.5;
+        System.out.println(total);
+    }
 }
