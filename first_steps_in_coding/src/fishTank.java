7f9995ab14ae59f98026eac5ba5bab82dd31fa64
BobsunPower <bobsunpower@gmail.com>
2022-04-23 07:57:06 +0300

    Completed

--- first_steps_in_coding/src/fishTank.java
+++ first_steps_in_coding/src/fishTank.java
@@ -1,2 +1,8 @@
+import java.util.Scanner;
+
 public class fishTank {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println((Double.parseDouble(scanner.nextLine()) * Double.parseDouble(scanner.nextLine()) * Double.parseDouble(scanner.nextLine())) * 0.001 * (1 - (Double.parseDouble(scanner.nextLine()) / 100)));
+    }
 }
