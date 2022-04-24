0dde463c02cb6a7f8ea478225ef80c70824328b8
BobsunPower <bobsunpower@gmail.com>
2022-04-24 09:53:57 +0300

    Completed

--- conditional_statements/src/excellentResult.java
+++ conditional_statements/src/excellentResult.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class excellentResult {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int grade = Integer.parseInt(scanner.nextLine());
-        if (grade >= 5.00) {
+        if (Integer.parseInt(scanner.nextLine()) >= 5.00) {
             System.out.println("Excellent!");
         }
     }
