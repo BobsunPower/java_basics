b3a21417f238b388ac6e10da50ecbbd407ac35df
BobsunPower <bobsunpower@gmail.com>
2022-04-24 09:59:52 +0300

    Completed

--- conditional_statements/src/greaterNumber.java
+++ conditional_statements/src/greaterNumber.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class greaterNumber {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        int fst = Integer.parseInt(scanner.nextLine());
+        int sec = Integer.parseInt(scanner.nextLine());
+        System.out.println(Math.max(fst, sec));
+    }
 }
