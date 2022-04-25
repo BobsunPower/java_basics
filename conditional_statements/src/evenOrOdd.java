8fca5f21a728dd1de7f27f0648d2cca9dc670c83
BobsunPower <bobsunpower@gmail.com>
2022-04-25 05:14:52 +0300

    Completed

--- conditional_statements/src/evenOrOdd.java
+++ conditional_statements/src/evenOrOdd.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class evenOrOdd {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        if (Integer.parseInt(scanner.nextLine()) % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
 }
