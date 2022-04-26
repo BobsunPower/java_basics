ebcc733c0177b545d638bfa1055986513905ba10
BobsunPower <bobsunpower@gmail.com>
2022-04-26 06:44:13 +0300

    Completed

--- conditional_statements/src/passwordGuess.java
+++ conditional_statements/src/passwordGuess.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class passwordGuess {
     public static void main(String[] arg) {
         Scanner scanner = new Scanner(System.in);
-        String password = scanner.nextLine();
-        if (Objects.equals(password, "s3cr3t!P@ssw0rd")) {
+        String pwd = scanner.nextLine();
+        if (Objects.equals(pwd, "s3cr3t!P@ssw0rd")) {
             System.out.println("Welcome");
         } else {
             System.out.println("Wrong password!");
