04d4434fcc28b322d594a90d3e78187c645d785b
BobsunPower <bobsunpower@gmail.com>
2022-04-26 06:41:38 +0300

    Completed

--- conditional_statements/src/passwordGuess.java
+++ conditional_statements/src/passwordGuess.java
@@ -1,2 +1,14 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class passwordGuess {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        String password = scanner.nextLine();
+        if (Objects.equals(password, "s3cr3t!P@ssw0rd")) {
+            System.out.println("Welcome");
+        } else {
+            System.out.println("Wrong password!");
+        }
+    }
 }
