1e938aadb90786ce16eba585e7aec1c62beedae0
BobsunPower <bobsunpower@gmail.com>
2022-04-29 10:07:17 +0300

    Completed

--- conditional_statements/src/areaOfFigures.java
+++ conditional_statements/src/areaOfFigures.java
@@ -1,2 +1,41 @@
+// Judge returns "Грешка при компилация" but it works and looks good.
+import java.util.Scanner;
+
 public class areaOfFigures {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        String figure = scanner.nextLine();
+        double area = switch (figure) {
+            case "square" -> Math.pow(Double.parseDouble(scanner.nextLine()), 2);
+            case "rectangle" -> Double.parseDouble(scanner.nextLine()) * Double.parseDouble(scanner.nextLine());
+            case "circle" -> Math.PI * (Math.pow(Double.parseDouble(scanner.nextLine()), 2));
+            case "triangle" -> (Double.parseDouble(scanner.nextLine()) * Double.parseDouble(scanner.nextLine()) / 2);
+            default -> 0;
+        };
+        System.out.printf("%.3f", area);
+    }
 }
+
+// Judge 100/100
+//public class areaOfFigures {
+//    public static void main(String[] arg) {
+//        Scanner scanner = new Scanner(System.in);
+//        String figure = scanner.nextLine();
+//        double area = 0;
+//        switch (figure) {
+//            case "square":
+//                area = Math.pow(Double.parseDouble(scanner.nextLine()), 2);
+//                break;
+//            case "rectangle":
+//                area = Double.parseDouble(scanner.nextLine()) * Double.parseDouble(scanner.nextLine());
+//                break;
+//            case "circle":
+//                area = Math.PI * (Math.pow(Double.parseDouble(scanner.nextLine()), 2));
+//                break;
+//            case "triangle":
+//                area = (Double.parseDouble(scanner.nextLine()) * Double.parseDouble(scanner.nextLine()) / 2);
+//                break;
+//        }
+//        System.out.printf("%.3f", area);
+//    }
+//}
