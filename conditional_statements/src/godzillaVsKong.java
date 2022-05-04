02dc5581cbc6d51bfd9253047651f1e83b38437d
BobsunPower <bobsunpower@gmail.com>
2022-05-04 20:51:56 +0300

    Completed

--- conditional_statements/src/godzillaVsKong.java
+++ conditional_statements/src/godzillaVsKong.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class godzillaVsKong {
+    public static void main(String[] arg) {
+        Scanner scan = new Scanner(System.in);
+        double budget = Double.parseDouble(scan.nextLine());
+        int people = Integer.parseInt(scan.nextLine());
+        double clothing = Double.parseDouble(scan.nextLine()) * people;
+        double decor = budget * 0.1;
+        if (people > 150) {
+            clothing *= 0.9;
+        }
+        double expenses = decor + clothing;
+        if (expenses > budget) {
+            System.out.printf("Not enough money!\nWingard needs %.2f leva more.", expenses - budget);
+        } else {
+            System.out.printf("Action!\nWingard starts filming with %.2f leva left.", budget - expenses);
+        }
+    }
 }
--- conditional_statements/src/toyShop.java
+++ conditional_statements/src/toyShop.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class toyShop {
-    public static void main(String[] args) {
+    public static void main(String[] arg) {
         Scanner scan = new Scanner(System.in);
         double holiday = Double.parseDouble(scan.nextLine());
         int qtyPuzzle = Integer.parseInt(scan.nextLine());
