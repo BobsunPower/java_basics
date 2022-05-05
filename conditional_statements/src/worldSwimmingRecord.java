77b6bb55a9a1c5d6e9ae8549b39d7937aac84484
BobsunPower <bobsunpower@gmail.com>
2022-05-05 21:32:34 +0300

    Completed

--- conditional_statements/src/worldSwimmingRecord.java
+++ conditional_statements/src/worldSwimmingRecord.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class worldSwimmingRecord {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        double recordSeconds = Double.parseDouble(scanner.nextLine());
+        double distanceMeters = Double.parseDouble(scanner.nextLine());
+        double meterSeconds = Double.parseDouble(scanner.nextLine());
+        double attemptSeconds = Math.floor(distanceMeters / 15) * 12.5 + distanceMeters * meterSeconds;
+        if (attemptSeconds < recordSeconds) {
+            System.out.printf("Yes, he succeeded! The new world record is %.2f seconds.", attemptSeconds);
+        } else {
+            double slower = (attemptSeconds - recordSeconds);
+            System.out.printf("No, he failed! He was %.2f seconds slower.", slower);
+        }
+    }
 }
