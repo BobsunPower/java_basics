5a0d3e3e09f195201690bc4084c48b954c9d9b0d
BobsunPower <bobsunpower@gmail.com>
2022-05-06 23:06:06 +0300

    Completed

--- conditional_statements/src/Shopping.java
+++ conditional_statements/src/Shopping.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class Shopping {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        double budget = Double.parseDouble(scanner.nextLine());
+        int qtyGC = Integer.parseInt(scanner.nextLine());
+        int qtyCPU = Integer.parseInt(scanner.nextLine());
+        int qtyRAM = Integer.parseInt(scanner.nextLine());
+        int priceGC = qtyGC * 250;
+        double priceCPU = qtyCPU * (priceGC * 0.35);
+        double priceRAM = qtyRAM * (priceGC * 0.1);
+        double expenses = priceGC + priceCPU + priceRAM;
+        if (qtyGC > qtyCPU) {
+            expenses *= 0.85;
+        }
+        if (budget >= expenses) {
+            System.out.printf("You have %.2f leva left!", budget - expenses);
+        } else {
+            System.out.printf("Not enough money! You need %.2f leva more!", expenses - budget);
+        }
+    }
 }
