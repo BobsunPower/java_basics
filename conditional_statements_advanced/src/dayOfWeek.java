8ec49ab786dee1d3516d8d4c637edeece36b65db
BobsunPower <bobsunpower@gmail.com>
2022-05-08 21:20:47 +0300

    Completed

--- conditional_statements_advanced/src/dayOfWeek.java
+++ conditional_statements_advanced/src/dayOfWeek.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
 public class dayOfWeek {
+    public static void main(String[] arg) {
+        Scanner scan = new Scanner(System.in);
+        int num = Integer.parseInt(scan.nextLine());
+        String day;
+        switch(num) {
+            case 1: day = "Monday"; break;
+            case 2: day = "Tuesday"; break;
+            case 3: day = "Wednesday"; break;
+            case 4: day = "Thursday"; break;
+            case 5: day = "Friday"; break;
+            case 6: day = "Saturday"; break;
+            case 7: day = "Sunday"; break;
+            default: day = "Error"; break;
+        }
+        System.out.println(day);
+    }
 }
