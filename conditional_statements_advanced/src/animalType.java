cc9b9be8ca4e5c14ca9a63d7a7c737de3f0c296b
BobsunPower <bobsunpower@gmail.com>
2022-05-09 19:06:08 +0300

    Completed

--- conditional_statements_advanced/src/animalType.java
+++ conditional_statements_advanced/src/animalType.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class animalType {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        String animal = scanner.nextLine();
+        String specie;
+        switch(animal) {
+            case "dog":
+                specie = "mammal"; break;
+            case "crocodile":case "tortoise": case "snake":
+                specie = "reptile"; break;
+            default: specie = "unknown";
+        }
+        System.out.printf(specie);
+    }
 }
