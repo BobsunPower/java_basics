e3d717720d8cc88dd17a6c33c782286b784707f0
BobsunPower <bobsunpower@gmail.com>
2022-05-09 06:59:46 +0300

    Completed

--- conditional_statements_advanced/src/weekendOrWorkingDay.java
+++ conditional_statements_advanced/src/weekendOrWorkingDay.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
 public class weekendOrWorkingDay {
+    public static void main(String[] arg) {
+        Scanner scan = new Scanner(System.in);
+        String day = scan.nextLine();
+        String work;
+        switch(day) {
+            case "Monday": case "Tuesday": case "Wednesday": case "Thursday": case "Friday":
+                work = "Working day"; break;
+            case "Saturday": case "Sunday":
+                work = "Weekend"; break;
+            default:
+                work = "Error";
+        }
+        System.out.println(work);
+    }
 }
