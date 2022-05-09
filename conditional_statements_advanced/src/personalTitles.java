1b6d336247af79aed96628a482de608c6e0a106f
BobsunPower <bobsunpower@gmail.com>
2022-05-09 19:29:22 +0300

    Completed

--- conditional_statements_advanced/src/personalTitles.java
+++ conditional_statements_advanced/src/personalTitles.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
 public class personalTitles {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        double age = Double.parseDouble(scanner.nextLine());
+        String sex = scanner.nextLine();
+        if (age >= 16 && sex.equals("m")) {
+            System.out.print("Mr.");
+        } else if (age < 16 && sex.equals("m")) {
+            System.out.print("Master");
+        } else if (age >= 16 && sex.equals("f")) {
+            System.out.print("Ms.");
+        } else if (age < 16 && sex.equals("f")) {
+            System.out.print("Miss");
+        }
+    }
 }
