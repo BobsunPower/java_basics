48f2e965ce76ac3dba2dc2902a344407fb8fa8aa
BobsunPower <bobsunpower@gmail.com>
2022-05-10 23:35:49 +0300

    Completed

--- conditional_statements_advanced/src/workingHours.java
+++ conditional_statements_advanced/src/workingHours.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
 public class workingHours {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        int hour = Integer.parseInt(scanner.nextLine());
+        String day = scanner.nextLine();
+        switch(day) {
+            case "Sunday": System.out.println("closed"); break;
+            case "Monday": case "Tuesday": case "Wednesday": case "Thursday": case "Friday": case "Saturday":
+                if (hour >= 10 && hour <= 18) {
+                    System.out.println("open");
+                } else {
+                    System.out.println("closed"); break;
+                }
+        }
+    }
 }
