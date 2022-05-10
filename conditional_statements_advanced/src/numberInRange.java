a9e4668cb54922ec204336b6dee40bcf82bc6701
BobsunPower <bobsunpower@gmail.com>
2022-05-10 10:12:25 +0300

    Completed

--- conditional_statements_advanced/src/numberInRange.java
+++ conditional_statements_advanced/src/numberInRange.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
 public class numberInRange {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        String inRange = inRange = num >= -100 && num <= 100 && num != 0 ? "Yes" : "No";
+        System.out.print(inRange);
+    }
 }
