128bf5059c0768f852a245dd64365f6262ce1390
BobsunPower <bobsunpower@gmail.com>
2022-05-11 20:59:10 +0300

    Completed

--- conditional_statements_advanced/src/invalidNumber.java
+++ conditional_statements_advanced/src/invalidNumber.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
 public class invalidNumber {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        if ((num < 100 || num > 200) && num != 0) {
+            System.out.println("invalid");
+        }
+    }
 }
