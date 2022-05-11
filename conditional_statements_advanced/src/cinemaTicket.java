c5ec257962770194c59d07c59d2f97b41cf14c6f
BobsunPower <bobsunpower@gmail.com>
2022-05-11 20:09:39 +0300

    Completed

--- conditional_statements_advanced/src/cinemaTicket.java
+++ conditional_statements_advanced/src/cinemaTicket.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
 public class cinemaTicket {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        String day = scanner.nextLine();
+        switch(day) {
+            case "Monday": case "Tuesday": case "Friday": System.out.println(12); break;
+            case "Wednesday": case "Thursday": System.out.println(14); break;
+            case "Saturday": case "Sunday": System.out.println(16); break;
+        }
+    }
 }
