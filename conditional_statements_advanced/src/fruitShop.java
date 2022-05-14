83a5540c24028a02f19152d2a7578866e3fe77b8
BobsunPower <bobsunpower@gmail.com>
2022-05-14 12:33:30 +0300

    Completed

--- conditional_statements_advanced/src/fruitShop.java
+++ conditional_statements_advanced/src/fruitShop.java
@@ -5,7 +5,7 @@ public class fruitShop {
         String fruit = scanner.nextLine();
         String day = scanner.nextLine();
         double quantity = Double.parseDouble(scanner.nextLine());
-        double price = -1;
+        double price = 0;
         if ((day.equals("Monday") || day.equals("Tuesday") || day.equals("Wednesday") || day.equals("Thursday") || day.equals("Friday"))) {
             switch (fruit) {
                 case "banana": price = 2.5; break; case "apple": price = 1.2; break; case "orange": price = 0.85; break; case "grapefruit": price = 1.45; break;
@@ -17,7 +17,7 @@ public class fruitShop {
                 case "kiwi": price = 3; break; case "pineapple": price = 5.6; break; case "grapes": price = 4.2; break;
             }
         }
-        if (price != -1) {
+        if (price > 0) {
             System.out.printf("%.2f", price * quantity);
         } else {
             System.out.println("error");
