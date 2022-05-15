d34bb1393708aaf50587322e22989551e0004d3b
BobsunPower <bobsunpower@gmail.com>
2022-05-15 08:42:26 +0300

    Completed

--- conditional_statements_advanced/src/summerOutfit.java
+++ conditional_statements_advanced/src/summerOutfit.java
@@ -1,2 +1,47 @@
+import java.util.Scanner;
+
 public class summerOutfit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int temperature = Integer.parseInt(scanner.nextLine());
+        String dayPart = scanner.nextLine();
+        String clothes = "";
+        String shoes = "";
+        switch (dayPart) {
+            case "Morning":
+                if (temperature >= 10 && temperature <= 18) {
+                    clothes = "Sweatshirt";
+                    shoes = "Sneakers";
+                } else if (temperature > 18 && temperature <= 24) {
+                    clothes = "Shirt";
+                    shoes = "Moccasins";
+                } else if (temperature >= 25) {
+                    clothes = "T-Shirt";
+                    shoes = "Sandals";
+                } break;
+            case "Afternoon":
+                if (temperature >= 10 && temperature <= 18) {
+                    clothes = "Shirt";
+                    shoes = "Moccasins";
+                } else if (temperature > 18 && temperature <= 24) {
+                    clothes = "T-Shirt";
+                    shoes = "Sandals";
+                } else if (temperature >= 25) {
+                    clothes = "Swim Suit";
+                    shoes = "Barefoot";
+                } break;
+            case "Evening":
+                if (temperature >= 10 && temperature <= 18) {
+                    clothes = "Shirt";
+                    shoes = "Moccasins";
+                } else if (temperature > 18 && temperature <= 24) {
+                    clothes = "Shirt";
+                    shoes = "Moccasins";
+                } else if (temperature >= 25) {
+                    clothes = "Shirt";
+                    shoes = "Moccasins";
+                } break;
+        }
+        System.out.printf("It's %d degrees, get your %s and %s.", temperature, clothes, shoes);
+    }
 }
