ee916e0d4df7ae07e59313541a9c4cec9bb8843c
BobsunPower <bobsunpower@gmail.com>
2022-05-16 10:34:37 +0300

    Completed

--- conditional_statements_advanced/src/journey.java
+++ conditional_statements_advanced/src/journey.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class journey {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double budget = Double.parseDouble(scanner.nextLine());
+        String season = scanner.nextLine();
+        String destination = "Europe";
+        String placeToStay = "Hotel";
+        if (budget <= 100) {
+            destination = "Bulgaria";
+            if (season.equals("summer")) {
+                placeToStay = "Camp";
+                budget *= 0.3;
+            } else if (season.equals("winter")) {
+                budget *= 0.7;
+            }
+        } else if (budget > 100 && budget <= 1000) {
+            destination = "Balkans";
+            if (season.equals("summer")) {
+                placeToStay = "Camp";
+                budget *= 0.4;
+            } else if (season.equals("winter")) {
+                budget *= 0.8;
+            }
+        } else if (budget > 1000) {
+            budget *= 0.9;
+        }
+        System.out.printf("Somewhere in %s\n%s - %.2f", destination, placeToStay, budget);
+    }
 }
