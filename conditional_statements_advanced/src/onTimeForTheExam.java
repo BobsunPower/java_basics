1f48d644e36529e93a347ca371fb5dcbd78275d9
BobsunPower <bobsunpower@gmail.com>
2022-05-17 17:50:43 +0300

    Completed

--- conditional_statements_advanced/src/onTimeForTheExam.java
+++ conditional_statements_advanced/src/onTimeForTheExam.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class onTimeForTheExam {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int examHours = Integer.parseInt(scan.nextLine());
+        int examMinutes = Integer.parseInt(scan.nextLine());
+        int arrivalHours = Integer.parseInt(scan.nextLine());
+        int arrivalMinutes = Integer.parseInt(scan.nextLine());
+        int examTime = examHours * 60 + examMinutes;
+        int arrivalTime = arrivalHours * 60 + arrivalMinutes;
+        int timeGap = Math.abs(examTime - arrivalTime);
+        if (examTime < arrivalTime) {System.out.println("Late");
+            if (timeGap < 60) {System.out.printf("%d minutes after the start",timeGap % 60);}
+            else {System.out.printf("%d:%02d hours after the start", timeGap / 60, timeGap % 60);}}
+        else if (timeGap <= 30) {System.out.println("On time"); if (timeGap > 0) {System.out.printf("%d minutes before the start", timeGap);}}
+        else {System.out.println("Early");
+            if (timeGap < 60) {System.out.printf("%d minutes before the start",timeGap % 60);}
+            else {System.out.printf("%d:%02d hours before the start", timeGap / 60, timeGap % 60);}}
+    }
 }
--- conditional_statements_advanced/src/operationsBetweenNumbers.java
+++ conditional_statements_advanced/src/operationsBetweenNumbers.java
@@ -7,7 +7,7 @@ public class operationsBetweenNumbers {
         double sec = Integer.parseInt(scanner.nextLine());
         String sign = scanner.nextLine();
         double res = 0;
-        String evenOrOdd = "odd";
+        String evenOdd = "odd";
         switch(sign) {
             case "+": res = fst + sec; break;
             case "-": res = fst - sec; break;
@@ -15,10 +15,10 @@ public class operationsBetweenNumbers {
             case "/": res = fst / sec; break;
             case "%": res = fst % sec; break;
         }
-        if (res % 2 == 0) {evenOrOdd = "even";}
+        if (res % 2 == 0) {evenOdd = "even";}
         if (sec == 0 && (sign.equals("/") || sign.equals("%"))) {System.out.printf("Cannot divide %d by zero", fst);}
         else if (sign.equals("/")) {System.out.printf("%d %s %.0f = %.2f", fst, sign, sec, res);}
         else if (sign.equals("%")) {System.out.printf("%d %s %.0f = %.0f", fst, sign, sec, res);}
-        else {System.out.printf("%d %s %.0f = %.0f - %s", fst, sign, sec, res, evenOrOdd);}
+        else {System.out.printf("%d %s %.0f = %.0f - %s", fst, sign, sec, res, evenOdd);}
     }
 }
