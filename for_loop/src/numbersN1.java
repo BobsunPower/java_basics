99a86538d539d3245be0db71bcba0bded3112265
BobsunPower <bobsunpower@gmail.com>
2022-05-20 20:31:45 +0300

    Completed

--- for_loop/src/numbersN1.java
+++ for_loop/src/numbersN1.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class numbersN1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = n; i > 0; i--) {System.out.println(i);}
+    }
 }
