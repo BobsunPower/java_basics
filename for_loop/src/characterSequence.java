eed30a60f3d977fa26642e9b05a0e35233f9629a
BobsunPower <bobsunpower@gmail.com>
2022-05-21 18:15:52 +0300

    Completed

--- for_loop/src/characterSequence.java
+++ for_loop/src/characterSequence.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class characterSequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        for (int i = 0; i < text.length(); i++) {char symbol = text.charAt(i); System.out.println(symbol);}
+        String txt = scanner.nextLine();
+        for (int i = 0; i < txt.length(); i++) {char sbl = txt.charAt(i); System.out.println(sbl);}
     }
 }
--- for_loop/src/vowelsSum.java
+++ for_loop/src/vowelsSum.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class vowelsSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String txt = scanner.nextLine();
+        int vlu = 0;
+        for (int i = 0; i < txt.length(); i++) {
+            char sbl = txt.charAt(i);
+            if (sbl == 'a') {vlu += 1;}
+            else if (sbl == 'e') {vlu += 2;}
+            else if (sbl == 'i') {vlu += 3;}
+            else if (sbl == 'o') {vlu += 4;}
+            else if (sbl == 'u') {vlu += 5;
+            }
+        }
+        System.out.println(vlu);
+    }
 }
