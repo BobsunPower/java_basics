e875764a7428eff827e20d3b63fe039779972ef0
BobsunPower <bobsunpower@gmail.com>
2022-05-23 17:47:27 +0300

    Completed

--- for_loop/src/sumNumbers.java
+++ for_loop/src/sumNumbers.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class sumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int seq = Integer.parseInt(scanner.nextLine());
+        int vlu = 0;
+        for (int i = 0; i < seq; i++)
+        {int num = Integer.parseInt(scanner.nextLine());vlu += num;}
+        System.out.println(vlu);
+    }
 }
