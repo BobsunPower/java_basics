5e261d3cc40b0efa74e52207b9d7d340cf6cfc4b
BobsunPower <bobsunpower@gmail.com>
2022-05-26 21:40:56 +0300

    Completed

--- for_loop/src/numberSequence.java
+++ for_loop/src/numberSequence.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class numberSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int seq = Integer.parseInt(scanner.nextLine());
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0;i < seq; i++) {int num = Integer.parseInt(scanner.nextLine());
+            if (num > max) {max = num;}
+            if (num < min){min = num;}}
+        System.out.printf("Max number: %d%n", max);
+        System.out.printf("Min number: %d", min);
+    }
 }
