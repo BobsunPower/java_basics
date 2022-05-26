cb1c98c7d25d616ad331ebeec59847ba44981d2b
BobsunPower <bobsunpower@gmail.com>
2022-05-26 21:41:18 +0300

    Completed

--- for_loop/src/leftAndRightSum.java
+++ for_loop/src/leftAndRightSum.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class leftAndRightSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int seq = Integer.parseInt(scanner.nextLine());
+        int left = 0; int right = 0;
+        for (int i = 0; i < seq; i++) {int cnt = Integer.parseInt(scanner.nextLine());
+            left = left + cnt;}
+        for (int i = 0; i < seq; i++) {int currentNum = Integer.parseInt(scanner.nextLine());
+            right = right + currentNum;}
+        if (left == right) {System.out.printf("Yes, sum = %d", left);}
+        else {System.out.printf("No, diff = %d", Math.abs(left - right));}
+    }
 }
