a0803938d04440fc26994fb05f99c4d2b54c6fa3
BobsunPower <bobsunpower@gmail.com>
2022-05-26 22:02:26 +0300

    Completed

--- for_loop/src/numbersEndingIn7.java
+++ for_loop/src/numbersEndingIn7.java
@@ -1,6 +1,5 @@
 public class numbersEndingIn7 {
     public static void main(String[] args) {
-        for (int i = 7; i <= 997; i++) {if (i % 10 == 7) {System.out.println(i);}
-        }
+        for (int i = 7; i <= 997; i += 10) {System.out.println(i);}
     }
 }
