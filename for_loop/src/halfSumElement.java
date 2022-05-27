257798c5c732ce0f13484e347b112a22024a5af0
BobsunPower <bobsunpower@gmail.com>
2022-05-27 07:03:46 +0300

    Completed

--- for_loop/src/halfSumElement.java
+++ for_loop/src/halfSumElement.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class halfSumElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int seq = Integer.parseInt(scanner.nextLine());
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < seq; i++) {int cnt = Integer.parseInt(scanner.nextLine());
+            sum += cnt;
+            if (cnt > max) {max = cnt;}        }
+        sum -= max;
+        if (max == sum) {System.out.printf("Yes\nSum = %d", max);}
+        else {System.out.printf("No\nDiff = %d", Math.abs(max - sum));}
+    }
 }
