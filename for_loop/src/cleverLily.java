83dacf133aaffbdc7d3bcf1448dc3243c92cae51
BobsunPower <bobsunpower@gmail.com>
2022-05-27 07:36:31 +0300

    Completed

--- for_loop/src/cleverLily.java
+++ for_loop/src/cleverLily.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class cleverLily {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double age = Integer.parseInt(scanner.nextLine());
+        double washingMachine = Double.parseDouble(scanner.nextLine());
+        double money = Double.parseDouble(scanner.nextLine()) * (Math.floor(age / 2) + (age % 2));
+        for (int i = 1; i <= age; i++) {if (i % 2 == 0) {money += i * 5 - 1;}}
+        double dif = Math.abs(money - washingMachine);
+        if (money >= washingMachine) {System.out.printf("Yes! %.2f", dif);}
+        else {System.out.printf("No! %.2f", dif);}
+    }
 }
