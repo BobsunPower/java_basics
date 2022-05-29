8dd2a9b610b008da567374ce14508b6a9af3d292
BobsunPower <bobsunpower@gmail.com>
2022-05-29 14:55:10 +0300

    Completed

--- for_loop/src/trekkingMania.java
+++ for_loop/src/trekkingMania.java
@@ -1,8 +1,12 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class trekkingMania {
+    public static DecimalFormat df = new DecimalFormat("0.00");
+    public static String percentFormat(double vlu)
+    {return df.format(vlu) + "%";}
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double crews = Integer.parseInt(scanner.nextLine());
@@ -18,11 +22,11 @@ public class trekkingMania {
             else if (crew <= 25) {kilimanjaro += crew;}
             else if (crew <= 40) {k2 += crew;} else {everest += crew;}}
         List<String> lst= new ArrayList<>();
-        lst.add(Double.toString(musala / climbers * 100));
-        lst.add(Double.toString(montBlanc / climbers * 100));
-        lst.add(Double.toString(kilimanjaro / climbers * 100));
-        lst.add(Double.toString(k2 / climbers * 100));
-        lst.add(Double.toString(everest / climbers * 100));
+        lst.add(percentFormat(musala / climbers * 100));
+        lst.add(percentFormat(montBlanc / climbers * 100));
+        lst.add(percentFormat(kilimanjaro / climbers * 100));
+        lst.add(percentFormat(k2 / climbers * 100));
+        lst.add(percentFormat(everest / climbers * 100));
         lst.forEach(System.out::println);
     }
 }
