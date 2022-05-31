78274cf7c562866328886dfeb579bd9929927830
BobsunPower <bobsunpower@gmail.com>
2022-05-31 07:30:05 +0300

    Completed

--- while_loop/src/readText.java
+++ while_loop/src/readText.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class readText {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {String txt = scanner.nextLine();
+            if (txt.equals("Stop")) {break;}
+            System.out.println(txt);}
+    }
 }
--- while_loop/src/sequence2Kplus1.java
+++
@@ -1,2 +0,0 @@
-public class sequence2Kplus1 {
-}
