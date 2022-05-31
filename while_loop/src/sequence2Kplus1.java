15d114b65e4579ec4509f628d0a425f8d58314b4
BobsunPower <bobsunpower@gmail.com>
2022-05-31 07:31:24 +0300

    Completed

---
+++ while_loop/src/sequence2Kplus1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class sequence2Kplus1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String value = null;
+        int cap = Integer.parseInt(scanner.nextLine()); int sum = 1;
+        while (true) {System.out.println(sum); sum = sum * 2 + 1; if (sum > cap) {break;}}}
+}
