1e8548f387d0b13644e8f733dfa54eaf9d78325b
BobsunPower <bobsunpower@gmail.com>
2022-06-03 14:19:23 +0300

    Completed

--- while_loop/src/accountBalance.java
+++ while_loop/src/accountBalance.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class accountBalance {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String mny = scan.nextLine();
+        double sum = 0;
+        while (!mny.equals("NoMoreMoney")) {
+            double qty = Double.parseDouble(mny);
+            if (qty < 0) {System.out.println("Invalid operation!"); break;}
+            System.out.printf("Increase: %.2f%n", qty);
+            sum += qty; mny = scan.nextLine();}
+        System.out.printf("Total: %.2f", sum);
+    }
 }
