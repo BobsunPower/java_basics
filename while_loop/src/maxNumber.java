18d744d0776473e693ae008e098ee282b412a2ab
BobsunPower <bobsunpower@gmail.com>
2022-06-06 23:16:57 +0300

    Completed

--- while_loop/src/maxNumber.java
+++ while_loop/src/maxNumber.java
@@ -1,4 +1,14 @@
 import java.util.Scanner;
 
 public class maxNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.nextLine();
+        int mxm = Integer.MIN_VALUE;
+        while (!num.equals("Stop"))
+        {int cnt = Integer.parseInt(num);
+            if (cnt > mxm) {mxm = cnt;}
+            num = scanner.nextLine();}
+        System.out.println(mxm);
     }
+}
