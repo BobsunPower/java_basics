1e86638fc7b544da8aa526f1c97ef03135b8f1e2
BobsunPower <bobsunpower@gmail.com>
2022-06-08 20:03:40 +0300

    Completed

--- while_loop/src/minNumber.java
+++ while_loop/src/minNumber.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class minNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.nextLine();
+        int mnm = Integer.MAX_VALUE;
+        while (!num.equals("Stop"))
+        {int cnt = Integer.parseInt(num);
+            if (cnt < mnm) {mnm = cnt;}
+            num = scanner.nextLine();}
+        System.out.println(mnm);
+    }
 }
