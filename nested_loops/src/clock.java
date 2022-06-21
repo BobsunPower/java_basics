d8bdcea80980c24c49f88ec553e3b999b6db5fd7
BobsunPower <bobsunpower@gmail.com>
2022-06-21 09:30:24 +0300

    Completed

---
+++ nested_loops/src/clock.java
@@ -0,0 +1,7 @@
+public class clock {
+    public static void main(String[] args) {
+        int hour = 0;
+        while (hour <= 23) {int minute = 0;
+            while (minute <= 59) {System.out.println(hour + ":" + minute); minute++;}
+            hour++;}}
+}
