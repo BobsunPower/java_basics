1f3ee8d4264084de20296e60da9a4e4014dc9e7a
BobsunPower <bobsunpower@gmail.com>
2022-06-21 09:26:56 +0300

    Completed

--- while_loop/src/coins.java
+++ while_loop/src/coins.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class coins {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double change = Double.parseDouble(scan.nextLine());
+        int coins = 0;
+        change = Math.floor(change * 100);
+        while (change > 0)
+        {if (change >= 200) {change -= 200; coins++;}
+        else if (change >= 100) {change -= 100; coins++;}
+        else if (change >= 50) {change -= 50; coins++;}
+        else if (change >= 20) {change -= 20; coins++;}
+        else if (change >= 10) {change -= 10;coins++;}
+        else if (change >= 5) {change -= 5; coins++;}
+        else if (change >= 2) {change -= 2; coins++;}
+        else if (change >= 1) {change -= 1; coins++;}}
+        System.out.println(coins);
+    }
 }
